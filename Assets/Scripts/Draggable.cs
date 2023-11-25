1478dc8cf7938ce3812dfa17ded7a6c085871bbf
Vik0t <89171514+Vik0t@users.noreply.github.com>
2023-11-25 19:22:04 +0700

    sample menu added

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IDragHandler
+{
+    public Canvas canvas;
+    private RectTransform rectTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
