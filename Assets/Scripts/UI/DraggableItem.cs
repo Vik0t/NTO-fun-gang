76bc534224f43aa4158a059a28282b9b5383601c
Vik0t <89171514+Vik0t@users.noreply.github.com>
2023-11-25 19:57:08 +0700

    UI improvement

--- Assets/Scripts/UI/DraggableItem.cs
+++ Assets/Scripts/UI/DraggableItem.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Draggable : MonoBehaviour, IDragHandler
+public class DraggableItem : MonoBehaviour, IDragHandler
 {
     public Canvas canvas;
     private RectTransform rectTransform;
