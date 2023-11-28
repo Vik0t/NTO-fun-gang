f1384e328cd7c18c76f71fdfefaea26c176153c9
Marat <maratdiarov200619@gmail.com>
2023-11-28 15:50:58 +0500

    Updated drone functions

--- Assets/Scripts/Drones/GroundBot.cs
+++ Assets/Scripts/Drones/GroundBot.cs
@@ -7,42 +7,52 @@ public class GroundBot : MonoBehaviour
     {
         public float speed;
         private int groundLayer;
+        private int interactiveLayer;
         private Rigidbody2D rb;
         private Animator anim;
         public Transform origin;
+        public Transform PickUpOrigin;
         private int dir;
-        public List<string> commandList;
+        public List<int> commandList;
+        public int jumpForce;
+        private bool alreadyCarryOn = false;
 
         void Start() {
             groundLayer = LayerMask.GetMask("Ground");
+            interactiveLayer = LayerMask.GetMask("Interactive");
             rb = gameObject.GetComponent<Rigidbody2D>();
             anim = gameObject.GetComponent<Animator>();
             
             if (transform.rotation.y == 0) dir = 1;
             else dir = -1;
 
-            //AcceptCommandList(commandList);
-            StartCoroutine(MainStart());
+            StartDoCommand(commandList);
         }
 
-        IEnumerator MainStart() {
-            yield return Move();
-            yield return Rotate();
-            yield return Move();
-        }
-
-        public void ToAcceptCommandList(List<string> commands) {
+        public void StartDoCommand(List<int> commands) {
             StartCoroutine(AcceptCommandList(commandList));
         }
 
-        IEnumerator AcceptCommandList(List<string> commands) {
+        IEnumerator AcceptCommandList(List<int> commands) {
             for (int i = 0; i < commands.Count; i++) {
                 switch (commands[i]) {
-                    case "Move":
+                    case 0:
                         yield return Move();
                         break;
-                    case "Rotate":
+                    case 1:
                         yield return Rotate();
+                        break;
+                    case 2:
+                        yield return Jump();
+                        break;
+                    case 3:
+                        yield return Pick();
+                        break;
+                    case 4:
+                        yield return Put();
+                        break;
+                    case 5:
+                        yield return Attack();
                         break;
                 }
             }
@@ -51,7 +61,7 @@ public class GroundBot : MonoBehaviour
         IEnumerator Rotate() {
             transform.Rotate(Vector2.up * 180);
             dir *= -1;
-            rb.velocity = new Vector2(speed * dir * 1, rb.velocity.y);
+            rb.velocity = new Vector2(speed * dir * 1.5f, rb.velocity.y);
             yield return null;
         }
 
@@ -60,18 +70,43 @@ public class GroundBot : MonoBehaviour
             RaycastHit2D hit;
 
             while (true) {
-                hit = Physics2D.Raycast(Vector2.left, Vector2.right, 1, groundLayer); 
-                if (dir == -1) hit = Physics2D.Raycast(Vector2.left, Vector2.right, 1, groundLayer); 
-                else if (dir == 1) hit = Physics2D.Raycast(Vector2.right, Vector2.right, 1, groundLayer); 
-                
+                if (dir == 1) hit = Physics2D.Raycast(origin.position, Vector2.right, 0.5f, groundLayer); 
+                else hit = Physics2D.Raycast(origin.position, Vector2.left, 0.5f); 
+
                 if (hit.collider != null) {
                     anim.Play("Idle"); 
-                    Debug.Log("Broken");
+                    Debug.Log("Ended");
                     break;
                 }
                 rb.velocity = new Vector2(speed * dir, rb.velocity.y);
                 yield return null;
             }
+        }
+
+        IEnumerator Jump() {
+            rb.velocity = new Vector2(speed * dir * 1.5f, jumpForce);
+            yield return null;
+        }
+
+        IEnumerator Pick() {
+            RaycastHit2D hit;
+            if (dir == 1) hit = Physics2D.Raycast(origin.position, Vector2.right, 0.5f, groundLayer); 
+            else hit = Physics2D.Raycast(origin.position, Vector2.left, 0.5f, groundLayer);
+
+            if (hit.collider.transform.gameObject.tag == "Pick" && !alreadyCarryOn) {
+                alreadyCarryOn = false;
+                hit.transform.position = PickUpOrigin.transform.position;
+                hit.transform.parent = PickUpOrigin.transform;
+            }
+            yield return null;
+        }
+
+        IEnumerator Put() {
+            yield return null;
+        }
+
+        IEnumerator Attack() {
+            yield return null;
         }
     }
 }
