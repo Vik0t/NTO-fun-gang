e8ceb502649efff18573cb4284d92861b75361c5
Marat <maratdiarov200619@gmail.com>
2023-12-02 10:58:59 +0500

    Updated programming panel

--- Assets/Scripts/UI/Programming/ApplyCommands.cs
+++ Assets/Scripts/UI/Programming/ApplyCommands.cs
@@ -8,18 +8,30 @@ namespace Muratich {
         private GroundBot groundBot;
         private FlightBot flightBot;
         public List<int> cmds;
+        private int currentBotInd;
 
         private void Start() {
             groundBot = GameObject.FindGameObjectWithTag("GroundBot").GetComponent<GroundBot>();
             flightBot = GameObject.FindGameObjectWithTag("FlightBot").GetComponent<FlightBot>();
         }
 
-        public void ApplyForGround() {
-            groundBot.StartDoCommands(cmds);
+        public void Apply() {
+            if (currentBotInd == 0) groundBot.StartDoCommands(cmds);
+            else if (currentBotInd == 1) flightBot.StartDoCommands(cmds);
         }
 
-        public void ApplyForFlight() {
-            flightBot.StartDoCommands(cmds);
+        public void ChangeBot() {
+            currentBotInd += 1;
+            if (currentBotInd >= 2) currentBotInd = 0;
+            UpdateUI();
+        }
+
+        private void UpdateUI() {
+
+        }
+
+        public void Delete() {
+            
         }
     }
 }
